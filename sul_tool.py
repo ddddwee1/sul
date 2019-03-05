08182b43f473ce7f4bb5adfba6df310393750712
Cheng Yu <cy960823@outlook.com>
2019-03-05 15:34:10 +0800

    Update SULTOOL

--- sul_tool.py
+++ sul_tool.py
@@ -1,23 +1,29 @@
 import cv2
 import numpy as np 
 import os 
+import time 
 
 def extract_frames(fname,prefix,skip=1):
 	print('Extracting %s'%fname)
+	t1 = time.time()
 	cap = cv2.VideoCapture(fname)
 	cnt = 0
-	imgnames = []
+	images = []
 	while cap.isOpened():
 		ret, frame = cap.read()
 		if not ret:
 			break
 		if cnt%skip==0:
-			imgname = prefix+'_%08d.jpg'%cnt
-			if not os.path.exists(imgname):
-				cv2.imwrite(imgname, frame)
-			imgnames.append(imgname)
+			if prefix is None:
+				images.append(frame)
+			else:
+				imgname = prefix+'_%08d.jpg'%cnt
+				if not os.path.exists(imgname):
+					cv2.imwrite(imgname, frame)
 		cnt += 1
-	return imgnames
+	t2 = time.time()
+	print('Extraction finished. Time elapsed:', t2-t1)
+	return images
 
 class video_saver():
 	def __init__(self,name,size, frame_rate=15.0):
