d83dea02f700282d994c7b1e478d152c4bf872f2
Cheng Yu <cy960823@outlook.com>
2019-05-07 08:56:33 +0800

    Update human pose detector

--- example/HumanPoseDetector/README.md
+++ example/HumanPoseDetector/README.md
@@ -1,9 +1,18 @@
 # Human Pose Detector
 
-Detect the human pose in coco format
+Detect the human pose in 2D. An implementation of [Simple Baselines for Human Pose Estimation and Tracking](https://arxiv.org/abs/1804.06208).
+
+'train.py' for training and 'main.py' for testing.
+
+### Requirement
+
+Tensorflow 2.0. See main page to install.
 
 ### link
 
-[pre-trained model](https://www.dropbox.com/s/pso6t0a6iyo6is1/posedetnet.zip?dl=0)
+[pre-trained model (COCO-17pts)](https://www.dropbox.com/s/pso6t0a6iyo6is1/posedetnet.zip?dl=0)
+
+[pre-trained model (H36M-19pts)](https://www.dropbox.com/s/xzyziudybfd3kpm/model_h36m_19pts.zip?dl=0)
 
 [Weight extraction code](https://www.dropbox.com/s/zhe9zabgdq7875x/dump_weights.zip?dl=0)
+
--- example/HumanPoseDetector/main.py
+++ example/HumanPoseDetector/main.py
@@ -2,11 +2,40 @@ import network
 import numpy as np 
 import tensorflow as tf 
 import model3 as M 
+import cv2 
+import matplotlib.pyplot as plt 
+
+def get_prediction(out, scale=4):
+	shape = out.shape
+	out = out.reshape([shape[0], shape[1]*shape[2], shape[3]])
+	scr = np.max(out, axis=1)
+	out = np.argmax(out, axis=1)
+
+	res = np.zeros([shape[0], shape[3], 2])
+	for i in range(shape[0]):
+		for j in range(shape[3]):
+			res[i,j,0] = out[i,j] % shape[2]
+			res[i,j,1] = out[i,j] // shape[1]
+	res = res * scale
+	return res, scr
+
+def plot(pts):
+	for i in range(len(pts)):
+		x = pts[i,0]
+		y = pts[i,1]
+		plt.plot(x,y,'o')
 
 tf.keras.backend.set_learning_phase(False)
-net = network.PosePredNet(17)
+net = network.PosePredNet(19)
 saver = M.Saver(net)
-saver.restore('./posedetnet/')
-x = np.ones([1,256, 192, 3]).astype(np.float32)
-y = net(x)
-print(y[0,10,10])
+saver.restore('./model/')
+
+img2 = cv2.imread('00000000.jpg')
+img = np.float32([img2])
+out = net(img).numpy()
+pts = get_prediction(out)[0]
+# print(pts.shape)
+
+plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+plot(pts[0])
+plt.show()
